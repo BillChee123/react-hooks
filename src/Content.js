feaee8be7a3563e116ec6bf66697e886c82ae14c
BillChee123 <colo316@hotmail.com>
2022-05-26 00:21:46 +0800

    Tidy code

--- src/Content.js
+++ src/Content.js
@@ -1,5 +1,4 @@
 import React, { useContext } from 'react';
-import { useState } from "react"
 import { Button } from 'react-bootstrap';
 import { AltElementsContext, CounterContext } from './App';
 
@@ -8,16 +7,14 @@ export default function Content () {
     const altElementList = useContext(AltElementsContext)
     const {counterList, setCounterList} = useContext(CounterContext);
     
-    const [currState, setState] = useState("0");
     const handleClick = () => {
         counterList[0] += 1
         setCounterList([...counterList])
     }
     return (
         <>
-            <Button onClick={handleClick}>Press me to update global list {`${counterList}`}</Button>
+            <Button onClick={handleClick}>Press me to update global list</Button>
             <div>{`${counterList}`}</div>
-            <div>{currState}</div>
             {altElementList}
         </>
     )
