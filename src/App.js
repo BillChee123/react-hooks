7bbcae977a27aa5f9c157b41de51acaef6422167
BillChee123 <colo316@hotmail.com>
2022-06-03 00:23:33 +0800

    Explore ResizeObserver

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React, { createContext, useEffect, useState } from 'react';
 import { Button, Form, Modal } from 'react-bootstrap';
 import Content from './Content';
 
-export const AltElementsContext = createContext()
-export const CounterContext = createContext()
+export const AltElementsContext = createContext(null)
+export const CounterContext = createContext(null)
 
 function AltElement(props) {
   const [isHighlighted, setHighlight] = useState(props.isHighlight);
@@ -23,15 +23,51 @@ function AltElement(props) {
   )
 }
 
+const reposition = (element, popOver) => {
+  console.log(
+    "fire reposition"
+  )
+  popOver.style.top = (element.offsetTop + element.offsetHeight - popOver.offsetHeight) + 'px';
+  popOver.style.left = (element.offsetLeft + element.offsetWidth - popOver.clientWidth) + 'px';
+  return;
+}
+
+const addMutationObserver = (element, attr) => {
+  const trackerAttr = 'has-tracker';
+  if (element.hasAttribute(trackerAttr)) {
+    return
+  }
+  element.setAttribute(trackerAttr, 'on');
+  const popOver = document.createElement('div');
+  popOver.textContent = attr + ": " + element.getAttribute(attr);
+  popOver.style.position = "absolute";
+  popOver.style.backgroundColor = "green";
+  element.insertAdjacentElement('beforebegin', popOver);
+  const observer = new ResizeObserver(() => reposition(element, popOver));
+  // Observe the lag
+  observer.observe(document.body)
+  observer.observe(element)
+  element.addEventListener('click', () => {
+    console.log(
+      'onclick'
+    )
+    element.width = element.width + 10
+    element.height = element.width + 20
+  })
+  popOver.style.top = (element.offsetTop + element.offsetHeight - popOver.offsetHeight) + 'px'
+  popOver.style.left = (element.offsetLeft + element.offsetWidth - popOver.clientWidth) + 'px';
+}
+
 function App() {
   const [altElementList, setAltElementList] = useState([]);
-  const [isModalToggled, setModalToggled] = useState(true);
+  const [isModalToggled, setModalToggled] = useState(false);
   const [counterList, setCounterList] = useState([3,1,2,3]);
   const updateAltElementList = () => {
     const altAttributes = ["aria-label", "cms-id", "alt"]
     const currList = [];
     altAttributes.forEach(attr => {
       document.querySelectorAll(`[${attr}]`).forEach((element) => {
+        addMutationObserver(element, attr)
         const altElement = <AltElement element={element} isHighlight={false} value={element.getAttribute(attr)}/>
         currList.push(altElement)
       });
@@ -64,7 +100,7 @@ function App() {
     console.log(document.getElementById('main-body'))
   }
   return (
-    <div id="main-body" className="App">
+    <div id="main-body" className="App" style={{position: "relative"}}>
       <Button onClick={addElement}>Click me to add element</Button>
       <div aria-label='Alternate Label 1'>Element with aria-label</div>
       <div>Element without aria-label</div>
@@ -72,8 +108,18 @@ function App() {
       <div>Element without aria-label</div>
       <div aria-label='Alternate Label 3'>Element with aria-label</div>
       <div>Element without aria-label</div>
+      {[...Array(1000).keys()].map((i) => {return <div aria-label={`Alternate Label '${i}'`}>Element with aria-label</div>})}
       <div cms-id="12345">Highlight me</div>
-      <img src="avatar.png" alt="Avatar" width="200" height="200"></img>
+      <picture>
+        <div style={{}}>
+          <img src="avatar.png" alt="Avatar1" width="200" height="200"></img>
+          <img src="avatar.png" alt="Avatar2" width="200" height="200"></img>
+          <img src="avatar.png" alt="Avatar3" width="200" height="200"></img>
+        </div>
+      </picture>
+          <img src="avatar.png" alt="Avatar4" width="200" height="200"></img>
+          <img src="avatar.png" alt="Avatar5" width="200" height="200"></img>
+          <img src="avatar.png" alt="Avatar6" width="200" height="200"></img>
       <div></div>
       <AltElementsContext.Provider value={altElementList}>
       <CounterContext.Provider value={{counterList, setCounterList}}>
